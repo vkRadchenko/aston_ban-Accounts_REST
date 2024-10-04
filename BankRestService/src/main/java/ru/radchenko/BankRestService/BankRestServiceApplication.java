0edbbb121eb52e9d449486063e7f893cdb020bf0
Vladimir Radchenko <vk.rad4enko@gmail.com>
2024-10-04 17:32:22 +0300

    init: initial project

---
+++ BankRestService/src/main/java/ru/radchenko/BankRestService/BankRestServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.radchenko.BankRestService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankRestServiceApplication.class, args);
+	}
+
+}
---
+++ BankRestService/src/test/java/ru/radchenko/BankRestService/BankRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.radchenko.BankRestService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankRestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
