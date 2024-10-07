6a4e878610b478a0a494c2ec90fd17607ce04281
Vladimir Radchenko <vk.rad4enko@gmail.com>
2024-10-07 13:17:18 +0300

    feat: create enum and add 3 state

---
+++ BankRestService/src/main/java/ru/radchenko/BankRestService/models/TransactionType.java
@@ -0,0 +1,10 @@
+package ru.radchenko.BankRestService.models;
+
+public enum TransactionType {
+//  Внесение средств на счет
+    DEPOSIT,
+//  Снятие средств
+    WITHDRAWAL,
+//  Перевод средств между счетами
+    TRANSFER
+}
