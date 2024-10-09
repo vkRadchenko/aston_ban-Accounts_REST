fcf05750cc00dde0752f579a964ba6b04f2644bc
Vladimir Radchenko <vk.rad4enko@gmail.com>
2024-10-09 20:47:39 +0300

    feat: add text

--- BankRestService/src/main/java/ru/radchenko/BankRestService/BankRestServiceApplication.java
+++ BankRestService/src/main/java/ru/radchenko/BankRestService/BankRestServiceApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BankRestServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BankRestServiceApplication.class, args);
+		System.out.println("My first springBoot app");
 	}
 
 }
