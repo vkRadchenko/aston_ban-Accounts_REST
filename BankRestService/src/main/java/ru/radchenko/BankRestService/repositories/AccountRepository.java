b21c0fa335276c42487ddbfe1099ea5f015b6c64
Vladimir Radchenko <vk.rad4enko@gmail.com>
2024-10-09 20:47:09 +0300

    fix: add Optional

--- BankRestService/src/main/java/ru/radchenko/BankRestService/repositories/AccountRepository.java
+++ BankRestService/src/main/java/ru/radchenko/BankRestService/repositories/AccountRepository.java
@@ -3,6 +3,8 @@ package ru.radchenko.BankRestService.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.radchenko.BankRestService.models.Account;
 
+import java.util.Optional;
+
 public interface AccountRepository extends JpaRepository<Account,Long> {
-    Account findByAccountNumber(String accountNumber);
+    Optional<Account> findByAccountNumber(String accountNumber);
 }
